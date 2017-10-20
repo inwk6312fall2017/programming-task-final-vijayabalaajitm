66148b5e645151bde79bed6622d17953a997e522
vijayabalaajitm <31935059+vijayabalaajitm@users.noreply.github.com>
2017-10-20 15:58:25 -0300

    changing cisco config file
    changing cisco config file from a file

--- changinciscoconfig.py
+++ changinciscoconfig.py
@@ -1,16 +1,32 @@
-
+from __future__ import print_function
+from netmiko import ConnectHandler
+import sys
+import time
 import select
 import paramiko
 import re
-fd = open(r'C:\NewdayTest.txt','w') 
+fd = open(r'C:\NewdayTest.txt','w') # Where you want the file to save to.
 old_stdout = sys.stdout   
 sys.stdout = fd 
 platform = 'cisco_ios'
-username = 'username' 
-password = 'password' 
+username = 'username' # edit to reflect
+password = 'password' # edit to reflect
 
-ip_add_file = open(r'C:\IPAddressList.txt','r') you want to connect to each one on a new line
+ip_add_file = open(r'C:\IPAddressList.txt','r') # a simple list of IP addresses you want to connect to each one on a new line
 
 for host in ip_add_file:
     host = host.strip()
-    fd.close()
+    device = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
+    output = device.send_command('terminal length 0')
+    output = device.send_command('enable') #Editable to be what ever is needed
+    print('##############################################################\n')
+    print('...................CISCO COMMAND SHOW RUN OUTPUT......................\n')
+    output = device.send_command('sh run')
+    print(output)
+    print('##############################################################\n')
+    print('...................CISCO COMMAND SHOW IP INT BR OUTPUT......................\n')
+    output = device.send_command('sh ip int br')
+    print(output) 
+    print('##############################################################\n')
+
+fd.close()
