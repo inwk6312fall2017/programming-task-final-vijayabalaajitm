824bc56fbadc8d1d805bf679f8dbe95a932ce027
vijayabalaajitm <31935059+vijayabalaajitm@users.noreply.github.com>
2017-10-20 14:58:44 -0300

    weather
    I tried this on my own, I am not sure if this satisfies the question.
    but. I tried to work the short way out for this question using the list.

--- weather.py
+++ weather.py
@@ -12,3 +12,10 @@ while True:
   print(url)
   json_data = requests.get(url).json()
   
+  json_status = json_data['status']
+  print('API status: '+ jason_status)  
+  
+  if json_status == 'OK':
+    for each in jason_data['resultsaformatted_place = json_data['results'][4]['formatted_location'] "My logic is to print the first five results, by getting the first five values using the list"
+    print()
+    print(formatted_place)
