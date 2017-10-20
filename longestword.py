983894b855700394a52c44b133bd4284c4e282f1
vijayabalaajitm <31935059+vijayabalaajitm@users.noreply.github.com>
2017-10-20 15:51:03 -0300

    longest word in 3 different text files
    longest words in 3 different text files

--- longestword.py
+++ longestword.py
@@ -11,3 +11,33 @@ def find_words(file1):
                 yield word
 
 longest_word = max(find_words('qugteroda'), key=len)
+
+
+def find_words2(file2):
+   file2 = Counter(file2)
+
+    with open('file2', 'r') as handle:
+        for word in handle:
+            letters = Counter(word.strip())
+
+            if file2 >= letters:
+                yield word
+
+longest_word = max(find_words('qugteroda'), key=len)
+
+
+def find_words3(file3):
+    file3 = Counter(book3)
+
+    with open('file3', 'r') as handle:
+        for word in handle:
+            letters = Counter(word.strip())
+
+            if file3 >= letters:
+                yield word
+
+longest_word = max(find_words('qugteroda'), key=len)
+
+find_words(book1.txt)
+find_words2(book2.txt)
+find_words3(book3.txt)
